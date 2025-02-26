63c51427444c00c6923c00e88f99c81370213245
duchetty <duchettydevendra@gmail.com>
2025-02-26 21:08:16 +0530

    intial commit

---
+++ src/main/java/com/os/OrderServiceApplication.java
@@ -0,0 +1,18 @@
+package com.os;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class OrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceApplication.class, args);
+		System.out.println("Welcome to OrderService");
+	}	
+	
+}
