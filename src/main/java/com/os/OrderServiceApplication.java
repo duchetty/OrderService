1fd15150a63faf34b4ebf42a1f4b9963f5fe8fde
duchetty <duchettydevendra@gmail.com>
2025-02-27 16:01:17 +0530

    ordeservice update

--- src/main/java/com/os/OrderServiceApplication.java
+++ src/main/java/com/os/OrderServiceApplication.java
@@ -9,10 +9,6 @@ public class OrderServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OrderServiceApplication.class, args);
 		System.out.println("Welcome to OrderService");
-		int a=10;
-		int b=20;
-		int c=a+b;
-		System.out.println(c);
 	}	
 	
 }
