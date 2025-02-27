c810c185a5abf006fbf9a142d8720d60d751dee3
duchetty <duchettydevendra@gmail.com>
2025-02-27 22:16:47 +0530

    object are added

--- src/main/java/com/os/OrderServiceApplication.java
+++ src/main/java/com/os/OrderServiceApplication.java
@@ -1,5 +1,7 @@
 package com.os;
 
+import java.util.stream.Collectors;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +22,31 @@ public class OrderServiceApplication {
 		System.out.println("Completed");
 		System.out.println("Completed example of order service");
 		
+		String s1="listen";
+		String s2="silent";
+		
+		if(areAnagram(s1,s2))
+		{
+			System.out.println("it is an anagram"+ s1 +":"+ s2);
+		}
+		else
+		{
+			System.out.println("it is not anagram"+ s1 +":"+ s2);
+		}
+		
 	}	
+	public static boolean areAnagram(String s1, String s2)
+	{
+		if(s1.length()!=s2.length())
+		{
+			return false;
+		}
+		
+		String st1=s1.chars().sorted().mapToObj(c->String.valueOf((char)c)).collect(Collectors.joining());
+		String st2=s2.chars().sorted().mapToObj(c->String.valueOf((char)c)).collect(Collectors.joining());
+		 
+		return st1.equals(st2);
+	
+	}
 	
 }
