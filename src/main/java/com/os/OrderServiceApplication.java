0b6a338ae5465a48bf019d8a0571989bd5046fda
duchetty <147183367+duchetty@users.noreply.github.com>
2025-02-27 22:15:18 +0530

    Update OrderServiceApplication.java

--- src/main/java/com/os/OrderServiceApplication.java
+++ src/main/java/com/os/OrderServiceApplication.java
@@ -15,8 +15,9 @@ public class OrderServiceApplication {
 		System.out.println("Welcome to OrderService");
 		int a=10;
 		int b=20;
+		int d=a-b;
 		int c=a+b;
-		System.out.println(" c :"+c);
+		System.out.println(" c :"+c + "d :"+d);
 		System.out.println("Completed");
 		System.out.println("Completed example of order service");
 		
